058f0b6f8dab63dfdc8ae94e9f87f884746c5926
xunaxu <78756e61@gmail.com>
2016-08-18 17:08:41 +0930

    clean up deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,18 +1,7 @@
-#!/usr/bin/env bash
-set -e # halt script on error
+#references only for how to publish gh-pages
 
-bundle exec jekyll build
-# bundle exec travis-lint
-# bundle exec htmlproof ${HTML_FOLDER} --disable-external
-
-cd ${HTML_FOLDER}
-
-# config
-git config --global user.email "78756e61@gmail.com"
-git config --global user.name "xunaxu"
-
-# deploy
+cd _site
 git init
 git add --all
 git commit -m "Deploy to GitHub Pages"
-git push --force --quiet "https://${GH_TOKEN}@github.com/${GH_REF}" master:gh-pages
+git push --force --quiet "https://github.com/XunaXu/Jekyll-Iris.git" master:gh-pages
