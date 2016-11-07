1153fb584c448db706fe1fcdb1f98eda164be556
Daniel Wilczynski <wilczynski.dan.dw@gmail.com>
2016-11-07 11:53:09 +0700

    deploy.sh will be proper shell script
    you can just run ./deploy.sh from command line

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,16 @@
-#references only for how to publish gh-pages
+#!/bin/sh
 
-cd _site
-git init
-git add --all
-git commit -m "Deploy to GitHub Pages"
-git push --force --quiet "https://github.com/XunaXu/Jekyll-Iris.git" master:gh-pages
+#type commands below manually into terminal to deplot (remember to 'jekyll build' first)
+
+#cd _site
+#git init
+#git add --all
+#git commit -m "Deploy to GitHub Pages"
+#git push --force --quiet "https://github.com/XunaXu/Jekyll-Iris.git" master:gh-pages
+
+#or run this file as shell script
+#needs to set permissions 'chmod 751 deploy.sh' first
+
+cd "_site" && git "init" && git "add" "." && git "commit" "-m" "Deploy to GitHub Pages" && git "push" "--force" "--quiet" "https://github.com/Daunus/Daniel-Blog.git" "master:gh-pages"
+
+echo "done"
